521309ba538aad053fe8286d66f4643f859e36f8
hsisco <hysensisco@gmail.com>
2019-10-21 13:41:55 -0700

    Clean up unused vars

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -8,8 +8,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
