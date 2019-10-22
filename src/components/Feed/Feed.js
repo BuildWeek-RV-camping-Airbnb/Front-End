7b2d6fd4a03e15ee8ac5b4c13294771a9641df51
hsisco <hysensisco@gmail.com>
2019-10-22 14:33:17 -0700

    Delete unused vars

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,16 +1,6 @@
 import React from 'react';
 
 // Material UI 
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
@@ -81,7 +71,7 @@ export default function Feed() {
           <Locations/>
         </Container>
       {/* End Cards unit */}
-      
+
       </main>
 
       {/* Footer */}
