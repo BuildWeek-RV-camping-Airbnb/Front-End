9a94ac47681943abe0f7988a8f9f7095374b1ff8
hsisco <hysensisco@gmail.com>
2019-10-24 10:49:52 -0700

    PP materialUI imports and content

--- src/components/PropertyPage/PropertyPage.js
+++ src/components/PropertyPage/PropertyPage.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
 // Material UI
-import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 
 // Components
 import NavBar from '../../components/NavBar';
 import Footer from '../../components/Footer';
 import DatePicker from '../../components/PropertyPage/DatePicker';
+import { getThemeProps } from '@material-ui/styles';
 
 const useStyles = makeStyles(theme => ({
   heroContent: {
+    height: '40vh'
     // background: {props.image},
   },
   cardGrid: {
@@ -33,7 +37,6 @@ export default function PropertyPage() {
 
         {/* Hero unit */}
         <div className={classes.hero}>
-          <Container />
         </div>
           {/* End hero unit */}
 
@@ -46,7 +49,16 @@ export default function PropertyPage() {
         </Grid>
         <Grid item xs={6}>
           <div className={classes.form}>
-            <
+            <div className={classes.formHeader}>
+              <Typography className={classes.heading} variant="h5" align="left">
+                <span><h2>${props.rate} </h2><h4>per night</h4></span>
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                <Star />
+                {props.rating}
+              </Typography>
+            </div>
+            <Divider />
             <DatePicker />
           </div>
         </Grid>
