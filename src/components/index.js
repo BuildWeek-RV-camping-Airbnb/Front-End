14c2e92f30c2d1239d77c97da7591d68e39e071b
Mark Gowen <mark.gowen78@gmail.com>
2019-10-24 12:39:00 -0700

    removed properties from routes

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,5 @@ export { default as RouteWithLayout } from './RouteWithLayout';
 export { default as SignIn } from './SignIn';
 export { default as SignUp } from './SignUp';
 export { default as Feed } from './Feed/Feed'
-export { default as PropertyPage } './PropertyPage/PropertyPage'
+
 
